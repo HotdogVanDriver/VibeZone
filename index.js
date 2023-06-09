988484c6a11b0672371591ce651a2aeda6acc354
HotdogVanDriver <53360521+HotdogVanDriver@users.noreply.github.com>
2023-06-09 12:12:40 +1000

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const daysPassed = Math.floor((currentDate - new Date('2023-06-08')) / (1000 * 6
 const powers = {
   R1: { currentPower: 1000000, dailyIncrease: 100, percentIncrease: 0.04 },
   R2: { currentPower: 2000000, dailyIncrease: 200, percentIncrease: 0.06 },
-  R3: { currentPower: 4000000, dailyIncrease: 300, percentIncrease: 0.08 },
-  R4: { currentPower: 5000000, dailyIncrease: 400, percentIncrease: 0.10 },
+  R3: { currentPower: 3000000, dailyIncrease: 300, percentIncrease: 0.08 },
+  R4: { currentPower: 4000000, dailyIncrease: 400, percentIncrease: 0.10 },
 };
 
 for (let rank in powers) {
@@ -18,13 +18,16 @@ for (let rank in powers) {
     dailyIncrease *= (1 + powerData.percentIncrease);
   }
   powerData.currentPower += dailyIncrease * daysPassed;
+  powerData.currentPower = Math.round(powerData.currentPower / 100000) * 100000;  // Round to the nearest 100,000
   powers[rank] = powerData;
 }
 
+let messageContent = "Updated Power Requirements:\n\n";  // Add a header to the message
 const message = Object.entries(powers).map(([rank, { currentPower }]) => {
   return `${rank}: ${currentPower.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }).join('\n');
+messageContent += message;  // Add your table to the message content
 
-axios.post(webhookUrl, { content: message })
+axios.post(webhookUrl, { content: messageContent })  // Send the message to the channel
   .then(() => console.log('Message sent successfully'))
   .catch(err => console.error('Failed to send message:', err));
